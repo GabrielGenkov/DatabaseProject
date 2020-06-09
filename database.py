9c5064af5541698f86ee82b544aad8fba00aee96
GabrielGenkov <gabriel.genkov2002@gmail.com>
2020-06-09 20:43:11 +0300

    Added datebase
    Database with users and movies

---
+++ database.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+DB_name = 'database.db'
+
+conn = sqlite3.connect(DB_name)
+
+conn.cursor().execute('''
+CREATE TABLE IF NOT EXISTS Users
+    (
+		id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+		username	TEXT NOT NULL,
+		mail	TEXT NOT NULL UNIQUE,
+		password	TEXT NOT NULL,
+		birthday	DATE NOT NULL
+    )
+''')
+conn.cursor().execute('''
+CREATE TABLE IF NOT EXISTS Movies
+    (
+		id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+		title	TEXT NOT NULL,
+		director	TEXT NOT NULL,
+		ageLimit	INTEGER
+		
+    )
+''')
+conn.cursor().execute('''
+CREATE TABLE IF NOT EXISTS Third
+    (
+		id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+		movieId	INTEGER NOT NULL,
+		userId	INTEGER NOT NULL,
+		
+		FOREIGN KEY(movieId) REFERENCES Movies(id),
+		FOREIGN KEY(userId) REFERENCES Users(id)
+    )
+''')
+conn.commit()
+
+class DB:
+    def __enter__(self):
+        self.conn = sqlite3.connect(DB_name)
+        return self.conn.cursor()
+
+    def __exit__(self, type, value, traceback):
+        self.conn.commit()
