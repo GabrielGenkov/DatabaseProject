8ff05043c5f454c7a0acbb0233ba8b29156942cc
Svetlin Georfiev <svetlin2002@gmail.com>
2020-06-09 23:42:41 +0300

    Adding movies.py and users.py

--- users.py
+++ users.py
@@ -1,24 +1,32 @@
 from database import DB
-
-
+from movies import Movies
 class Users:
-	def __init__(self, id, username, mail, password, birthday):
-        	self.id = id
-        	self.username = username
-        	self.mail = mail
-        	self.password = password
-		self.birthday = birthday
+        
+        def __init__(self, id, username, mail, password, birthday):
+                self.id = id
+                self.username = username
+                self.mail = mail
+                self.password = password
+                self.birthday = birthday
 		
-	def create(self):
-        with DB() as db:
-            values = (self.username, self.mail, self.password, self.birthday)
-            row = db.execute('INSERT INTO posts (username, mail, password, birthday) VALUES (?, ?, ?, ?)', values)
-            return self
+        def create(self):
+                with DB() as db:
+                        values = (self.username, self.mail, self.password, self.birthday)
+                        row = db.execute('INSERT INTO Users (username, mail, password, birthday) VALUES (?, ?, ?, ?)', values)
+                return self
 	
-	def delete(self):
-        with DB() as db:
-            db.execute('DELETE FROM posts WHERE id = ?', (self.id,))
-
-
-	
-	
+        def delete(self):
+                with DB() as db:
+                        db.execute('DELETE FROM Users WHERE id = ?', (self.id,))
+                        
+        def userAssign(self, movie):
+                with DB() as db:
+                        values = (movie.id, self.id)
+                        row = db.execute('INSERT INTO Third (movieId, userId) VALUES(?, ?)', values)
+                return self
+                        
+        def userAllMovies(self):
+                with DB() as db:
+                        values = (self.id)
+                        row = db.execute('SELECT * FROM Movies INNER JOIN Third ON Movies.id = Third.movieId WHERE ? = userId', values).fetchall()
+                return [Movies(*row) for row in rows] 
