85089d9657809e95a38872b7873dec73706a2af3
KonstantinDonev <donev.konstantin@gmail.com>
2020-06-11 19:27:43 +0300

    added find director method and removed errors

--- movies.py
+++ movies.py
@@ -1,4 +1,6 @@
 from database import DB
+
+
 class Movies:
     
     def __init__(self, id, title, director, ageLimit, date):
@@ -11,29 +13,32 @@ class Movies:
     def create(self):
         with DB() as db:
             values = (self.title, self.director, self.ageLimit)
-            row = db.execute('INSERT INTO posts (title, director, ageLimit) VALUES (?, ?, ?)', values)
+            row = db.execute('INSERT INTO Movies (title, director, ageLimit) VALUES (?, ?, ?)', values)
         return self
     
     def delete(self):
         with DB() as db:
-            db.execute('DELETE FROM posts WHERE id = ?', (self.id,))
+            db.execute('DELETE FROM Movies WHERE id = ?', (self.id,))
 
     @staticmethod
     def dateExpired():
         with  DB() as db:
-                row = db.execute("SELECT * FROM Movies WHERE date < datetime('now')").fetchall()
+                rows = db.execute("SELECT * FROM Movies WHERE date < datetime('now')").fetchall()
         return [Movies(*row) for row in rows]
         
     @staticmethod
     def dateActive():
         with  DB() as db:
-            row = db.execute("SELECT * FROM Movies WHERE date >= datetime('now')").fetchall()
+            rows = db.execute("SELECT * FROM Movies WHERE date >= datetime('now')").fetchall()
         return [Movies(*row) for row in rows]
         
     @staticmethod
-	def edit(id, title):
+    def edit(id, title):
         with DB() as db:
-        	values = (title)
-            db.execute('''
-            UPDATE movies SET title = ? WHERE id = ?''', values, id)
-		return self
+            values = (title)
+            db.execute('UPDATE Movies SET title = ? WHERE id = ?', values, id)
+
+    def findDirector(self):
+        with DB() as db:
+            rows = db.execute('SELECT * FROM Movies WHERE directorId = ?', (self.director,)).fetchall()
+        return [Movies(*row) for row in rows]
